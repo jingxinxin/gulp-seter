8fef2f3faf2e6ed1c699b8cf3195dbe341fac13f
Jacks <191100479@qq.com>
2016-09-19 17:40:46 +0800

    初始化项目和添加两块gulp文件

---
+++ gulpconfig.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Jack·s on 2016/9/19.
+ */
+const d = '';
+
+
+module.exports = {
+  version: {
+    tasks: './gulp/tasks',
+    basic: './gulp/basic',
+    boyce: './gulp/boyce'
+  },
+  default: this.version
+};
